d05d0040d880e085d36647e33b85f015ba2c2ece
Alien <ihor.levitsky@gmail.com>
2025-04-21 15:47:06 +0300

    add Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+package MyFirst;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+    public int sub(int a, int b) {
+        return a-b;
+    }
+    public int mul (int a, int b) {
+        return a*b;
+    }
+    public int div(int a, int b) {
+        return a/b;
+    }
+
+    public int subminus(int a, int b) {
+        return -a-b;
+    }
+}
